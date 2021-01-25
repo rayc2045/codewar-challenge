662e4a29ce446d516551e95f961b80ffacee859f
Ray C <rayc2045@gmail.com>
2021-01-25 14:18:08 +0800

    Add file

---
+++ 10 - Duplicate Encoder/script.js
@@ -0,0 +1,43 @@
+/*
+The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+Examples
+
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
+*/
+
+function duplicateEncode(word) {
+  const count = {};
+  const letters = word.toLowerCase().split('');
+
+  letters.forEach((letter) => {
+    count[letter] = (count[letter] || 0) + 1;
+  });
+
+  return letters
+    .map(letter => count[letter] === 1 ? '(' : ')')
+    .join('');
+}
+
+// Alternates
+
+function duplicateEncode(word) {
+  var unique = '';
+  word = word.toLowerCase();
+
+  for (var i = 0; i < word.length; i++) {
+    word.lastIndexOf(word[i]) == word.indexOf(word[i])
+      ? (unique += '(')
+      : (unique += ')');
+  }
+  return unique;
+}
+
+const duplicateEncode = s => s
+  .toLowerCase()
+  .split('')
+  .map((e, _, a) => a.indexOf(e) === a.lastIndexOf(e) ? '(' : ')')
+  .join('');
